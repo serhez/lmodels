cc6b0e5deb7469b9cff9c840ec8e1c72351466a3
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-03-04 15:36:10 +0200

    fix: add `OpenAIModel` to package `__init__`

--- lmodels/__init__.py
+++ lmodels/__init__.py
@@ -1,9 +1,6 @@
 from .hf_model import HFModel
 from .mock_model import MockModel
 from .model import Model
+from .openai_model import OpenAIModel
 
-__all__ = [
-    "HFModel",
-    "MockModel",
-    "Model",
-]
+__all__ = ["HFModel", "MockModel", "Model", "OpenAIModel"]
