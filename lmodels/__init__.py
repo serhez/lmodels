10330ead33a13d13456fe4c92aca6f73e9ba280d
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-03-04 19:54:25 +0200

    fix: add `LlamaModel` to `__init__`

--- lmodels/__init__.py
+++ lmodels/__init__.py
@@ -1,6 +1,7 @@
 from .hf_model import HFModel
+from .llama_model import LlamaModel
 from .mock_model import MockModel
 from .model import Model
 from .openai_model import OpenAIModel
 
-__all__ = ["HFModel", "MockModel", "Model", "OpenAIModel"]
+__all__ = ["HFModel", "MockModel", "Model", "OpenAIModel", "LlamaModel"]
